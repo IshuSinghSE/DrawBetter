e38401222417859071561872396c28508d29d9fb
Arya Pratap Singh <notaryasingh@gmail.com>
2025-09-24 08:01:23 +0530

    loaders and configs more
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { Authenticated, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
 
 export default function Home() {
   return (
-   <Button>
-    Hello World
-   </Button>
+    <>
+      <Authenticated>
+        <UserButton />
+        <p> insider</p>
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+    </>
   );
 }
