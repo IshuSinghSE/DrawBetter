fe27763d0510bb35b53e0974e9c6b670397afeaf
Arya Pratap Singh <notaryasingh@gmail.com>
2025-09-27 01:00:23 +0530

    adding colors avatars and fallback
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = [
+  "#DC2626",
+  "#D97706",
+  "#CA8A04",
+  "#16A34A",
+  "#2563EB",
+  "#7C3AED",
+  "#DB2777",
+  "#F43F5E",
+  "#E11D48",
+  "#BE123C",
+]; //10
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
