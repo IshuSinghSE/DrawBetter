fdc25cfc78bc4edcb3c2db36986909af2070b874
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-01 00:21:17 +0530

    feat(canvas): add the zoom in and out functionality
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- app/draw/[drawId]/_components/selection-tools.tsx
+++ app/draw/[drawId]/_components/selection-tools.tsx
@@ -11,172 +11,119 @@ import { Hint } from "@/components/hint";
 import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 interface SelectionToolsProps {
-    camera: Camera;
-    setLastUsedColor : (color: Color) => void;
+  camera: Camera;
+  setLastUsedColor: (color: Color) => void;
 }
 
-export const SelectionTools = memo((
-    {
-        camera,
-        setLastUsedColor
-    }: SelectionToolsProps
-)=>{
+export const SelectionTools = memo(
+  ({ camera, setLastUsedColor }: SelectionToolsProps) => {
+    const selection = useSelf((me) => me?.presence.selection);
 
-    const selection = useSelf((me)=>me?.presence.selection);
-
-    const moveToBack = useMutation((
-        {storage}
-    )=>{
-
+    const moveToBack = useMutation(
+      ({ storage }) => {
         const liveLayerIds = storage.get("layerIds");
 
-        const indices : number[]= [];
+        const indices: number[] = [];
 
         const arr = liveLayerIds.toImmutable();
 
         for (let i = 0; i < arr.length; i++) {
-            if (selection.includes(arr[i])) {
-                indices.push(i);
-            }
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
         }
 
         for (let i = 0; i < indices.length; i++) {
-            liveLayerIds.move(indices[i], i);
-        
+          liveLayerIds.move(indices[i], i);
         }
+      },
+      [selection]
+    );
 
-
-    },[selection])
-
-    const moveToFront = useMutation((
-        {storage}
-    )=>{
-
+    const moveToFront = useMutation(
+      ({ storage }) => {
         const liveLayerIds = storage.get("layerIds");
 
-        const indices : number[]= [];
+        const indices: number[] = [];
 
         const arr = liveLayerIds.toImmutable();
 
         for (let i = 0; i < arr.length; i++) {
-            if (selection.includes(arr[i])) {
-                indices.push(i);
-            }
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
         }
 
         for (let i = indices.length - 1; i >= 0; i--) {
-            liveLayerIds.move(indices[i], arr.length - 1 - (indices.length - 1 - i));
+          liveLayerIds.move(
+            indices[i],
+            arr.length - 1 - (indices.length - 1 - i)
+          );
         }
+      },
+      [selection]
+    );
 
-
-    },[selection])
-
-
-
-
-
-
-
-    const setFill = useMutation((
-        {storage },
-        fill: Color
-    )=>{
-
+    const setFill = useMutation(
+      ({ storage }, fill: Color) => {
         const liveLayers = storage.get("layers");
         setLastUsedColor(fill);
 
-        selection.forEach((id)=>{
-            liveLayers.get(id)?.set("fill", fill);
+        selection.forEach((id) => {
+          liveLayers.get(id)?.set("fill", fill);
         });
-
-    },[selection, setLastUsedColor])
-
+      },
+      [selection, setLastUsedColor]
+    );
 
     const deleteLayers = useDeleteLayers();
-
-    
-
-
 
     const selectionBounds = useSelectionBounds();
 
     if (!selectionBounds) {
-        return null;
+      return null;
     }
 
-    const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
-    const y = selectionBounds.y + camera.y;
-
-
-
+    const x =
+      (selectionBounds.width / 2 + selectionBounds.x + camera.x) * camera.zoom;
+    const y = (selectionBounds.y + camera.y) * camera.zoom;
 
     return (
-        <div
+      <div
         className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
         style={{
-            transform: `translate(
+          transform: `translate(
                 calc(${x}px - 50%),
-                calc(${y-16}px - 100%)
+                calc(${y - 16}px - 100%)
             )`,
         }}
-        >
-           <ColorPicker
-           onChange={setFill}
-           />
-            
-            <div
-            className="flex flex-col gap-y-0.5 "
-            >
-                <Hint
-                label="Bring Forward"
-                >
-                    <Button
-                    onClick={moveToFront}
-                    variant={"draw"}
-                    size={"icon"}
-                    >
-                        <BringToFront />
-                    </Button>
+      >
+        <ColorPicker onChange={setFill} />
 
-                </Hint>
-                
-                <Hint
-                label="Send Backward"
-                side="bottom"
-                >
-                    <Button
-                    onClick={moveToBack}
-                    variant={"draw"}
-                    size={"icon"}
-                    >
-                        <SendToBack />
-                    </Button>
+        <div className="flex flex-col gap-y-0.5 ">
+          <Hint label="Bring Forward">
+            <Button onClick={moveToFront} variant={"draw"} size={"icon"}>
+              <BringToFront />
+            </Button>
+          </Hint>
 
-                </Hint>
-
-            </div>
-
-
-           <div
-           className="flex items-center border-l border-neutral-200 pl-2 ml-2"
-           >
-            <Hint 
-            label="Delete"
-            >
-                <Button
-                variant={"draw"}
-                size={"icon"}
-                onClick={deleteLayers}
-                >
-                    <Trash2 />
-
-                </Button>
-
-            </Hint>
-            
-           </div>
+          <Hint label="Send Backward" side="bottom">
+            <Button onClick={moveToBack} variant={"draw"} size={"icon"}>
+              <SendToBack />
+            </Button>
+          </Hint>
         </div>
+
+        <div className="flex items-center border-l border-neutral-200 pl-2 ml-2">
+          <Hint label="Delete">
+            <Button variant={"draw"} size={"icon"} onClick={deleteLayers}>
+              <Trash2 />
+            </Button>
+          </Hint>
+        </div>
+      </div>
     );
-});
+  }
+);
 
 SelectionTools.displayName = "SelectionTools";
