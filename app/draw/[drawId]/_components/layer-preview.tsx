b34eb770993e841b84de39b003a73507062a3d21
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-01 09:28:36 +0530

    chore(cleanups): removing unused thing
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- app/draw/[drawId]/_components/layer-preview.tsx
+++ app/draw/[drawId]/_components/layer-preview.tsx
@@ -24,67 +24,59 @@ export const LayerPreview = memo(
       return null;
     }
 
-    switch (layer.type) {
-
-        case LayerType.Path:
-        return (
-          <Path
-            key={id}
-            points={layer.points}
-            onPointerDown={(e) => onLayerPointerDown(e, id)}
-            x={layer.x}
-            y={layer.y}
-            fill={layer.fill? colorToCss(layer.fill) : "#000"}
-            stroke={selectionColor}
-
-          />
-        )
-
-
-        case LayerType.Note:
-        return (
-          <Note 
-             id={id}
-            layer={layer}
-            onPointerDown={onLayerPointerDown}
-            selectionColor={selectionColor}
-          />
-        )
-
-      case LayerType.Text:
-        return (
-          <Text 
-             id={id}
-            layer={layer}
-            onPointerDown={onLayerPointerDown}
-            selectionColor={selectionColor}
-          />
-        )
-
-
-      case LayerType.Ellipse:
-        return (
-          <Ellipse
-            id={id}
-            layer={layer}
-            onPointerDown={onLayerPointerDown}
-            selectionColor={selectionColor}
-          />
-        );
-
-      case LayerType.Rectangle:
-        return (
-          <Rectangle
-            id={id}
-            layer={layer}
-            onPointerDown={onLayerPointerDown}
-            selectionColor={selectionColor}
-          />
-        );
-      default:
-        console.warn("unknown layer type");
-        return null;
-    }
+  switch (layer.type) {
+    case LayerType.Path:
+      return (
+        <Path
+          key={id}
+          points={layer.points}
+          onPointerDown={(e) => onLayerPointerDown(e, id)}
+          x={layer.x}
+          y={layer.y}
+          fill={layer.fill ? colorToCss(layer.fill) : "#000"}
+          stroke={selectionColor}
+        />
+      );
+    case LayerType.Note:
+      return (
+        <Note
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      );
+    case LayerType.Text:
+      return (
+        <Text
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      );
+    case LayerType.Ellipse:
+      return (
+        <Ellipse
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      );
+    case LayerType.Rectangle:
+      return (
+        <Rectangle
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      );
+    default:
+      console.warn("unknown layer type");
+      return null;
+  }
   }
 );
 
